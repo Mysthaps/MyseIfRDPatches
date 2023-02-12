684b5795fdb9d66e7e9f0795bdd6baa62cf17e7c
MyseIf <67219032+HellUser0@users.noreply.github.com>
2023-02-12 19:59:19 +0700

    Update ShowFPS, added Legacy option

--- GarbagePlugins.cs
+++ GarbagePlugins.cs
@@ -17,14 +17,18 @@ namespace GarbagePlugins
         private static ConfigEntry<bool> configEnableOldSpeedChange;
         private static ConfigEntry<bool> configRankColorOnDoubleSpeed;
         private static ConfigEntry<bool> configDebugMode;
-        private static ConfigEntry<bool> configShowFPS;
+        private static ConfigEntry<ShowFpsOptions> configShowFPS;
+        private static ConfigEntry<bool> configFiveFourteen;
+
+        private enum ShowFpsOptions { Enabled, Legacy, Disabled }
 
         private void Awake()
         {
             configEnableOldSpeedChange = Config.Bind("General", "EnableOldSpeedChange", true, "Changes chili/ice speed to be 2x/.5x, respectively");
             configRankColorOnDoubleSpeed = Config.Bind("General", "RankColorOnDoubleSpeed", true, "Changes rank screen color if EnableOldSpeedChange is enabled");
             configDebugMode = Config.Bind("General", "DebugMode", false, "Enables Debug Mode");
-            configShowFPS = Config.Bind("General", "ShowFPS", true, "Shows FPS if Debug Mode is not active");
+            configShowFPS = Config.Bind("General", "ShowFPS", ShowFpsOptions.Enabled, "Shows FPS if Debug Mode is not active");
+            configFiveFourteen = Config.Bind("Funny", "FiveFourteen", false, "514");
             
             if (configEnableOldSpeedChange.Value)
                 Harmony.CreateAndPatchAll(typeof(EnableOldSpeedChange));
@@ -35,8 +39,11 @@ namespace GarbagePlugins
             if (configDebugMode.Value)
                 Harmony.CreateAndPatchAll(typeof(DebugMode));
 
-            if (configShowFPS.Value && !configDebugMode.Value)
+            if (configShowFPS.Value != ShowFpsOptions.Disabled)
                 Harmony.CreateAndPatchAll(typeof(ShowFPS));
+
+            if (configFiveFourteen.Value)
+                Harmony.CreateAndPatchAll(typeof(FiveFourteen));
 
             Logger.LogInfo($"Plugin is loaded!");
         }
@@ -90,11 +97,36 @@ namespace GarbagePlugins
             {
                 if (!RDC.debug){
                     __instance.debugText.gameObject.SetActive(true);
-                    if (scnGame.fps == 0.0) scnGame.fps = 1f / Time.unscaledDeltaTime;
-                    scnGame.fps = scnGame.fps * 0.99f + (1f / Time.unscaledDeltaTime) * 0.01f;
-                    __instance.debugText.text = "" + string.Format("<color=#ffffff>FPS:</color> <color={0}>{1}</color>", (object) (scnGame.fps < 30 ? "#ff0000" : (scnGame.fps < 50 ? "#ffff00" : "#00ff00")), (object) scnGame.fps);
+
+                    switch (configShowFPS.Value)
+                    {
+                        case ShowFpsOptions.Enabled:
+                            if (scnGame.fps == 0.0) scnGame.fps = 1f / Time.unscaledDeltaTime;
+                            scnGame.fps = scnGame.fps * 0.99f + (1f / Time.unscaledDeltaTime) * 0.01f;
+                            __instance.debugText.text = "" + string.Format("<color=#ffffff>FPS:</color> <color={0}>{1}</color>", (object) (scnGame.fps < 30 ? "#ff0000" : (scnGame.fps < 50 ? "#ffff00" : "#00ff00")), (object) (int) scnGame.fps);
+                            break;
+                        case ShowFpsOptions.Legacy:
+                            int num = Mathf.RoundToInt(1f / Time.unscaledDeltaTime);
+                            __instance.debugText.text = "" + string.Format("<color=#ffffff>FPS:</color> <color={0}>{1}</color>", (object) (num < 30 ? "#ff0000" : (num < 50 ? "#ffff00" : "#00ff00")), (object) num);
+                            break;
+                    }
+                    
                     __instance.currentLevel.Update();
                 }
+            }
+        }
+
+        private static class FiveFourteen
+        {
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(HUD), "ShowRankDescription")]
+            public static void Postfix(HUD __instance)
+            {
+                string str = "Mistakes: 514\n514 early + 514 late = 514 offset frames";
+                __instance.resultsSingleplayer.text = str;
+                __instance.resultsSingleplayer.gameObject.SetActive(true);
+                __instance.description.text = "514/514";
+                __instance.description.gameObject.SetActive(true);
             }
         }
     }
