1b2b35b8383df141fe69cb5a7e51197c48e69886
MyseIf <helluser0@gmail.com>
2023-03-21 22:55:08 +0700

    i did some stuff

--- GarbagePlugins.cs
+++ GarbagePlugins.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,27 +10,27 @@ using UnityEngine.UI;
 
 namespace GarbagePlugins
 {
-    [BepInPlugin("com.rhythmdr.garbageplugins", "Garbage Plugins", "1.0.0")]
+    [BepInPlugin("com.rhythmdr.garbageplugins", "Garbage Plugins", "1.3.0")]
     [BepInProcess("Rhythm Doctor.exe")]
     public class Plugin : BaseUnityPlugin
     {
         private static ConfigEntry<bool> configEnableOldSpeedChange;
         private static ConfigEntry<bool> configRankColorOnDoubleSpeed;
-        private static ConfigEntry<bool> configDebugMode;
         private static ConfigEntry<ShowFpsOptions> configShowFPS;
         private static ConfigEntry<bool> configFiveFourteen;
-        private static ConfigEntry<bool> configHMode;
+        private static ConfigEntry<bool> configSamuHrai;
+        private static ConfigEntry<bool> configShowAccuracy;
 
         private enum ShowFpsOptions { Enabled, Legacy, Disabled }
 
         private void Awake()
         {
-            configEnableOldSpeedChange = Config.Bind("General", "EnableOldSpeedChange", true, "Changes chili/ice speed to be 2x/.5x, respectively");
-            configRankColorOnDoubleSpeed = Config.Bind("General", "RankColorOnDoubleSpeed", true, "Changes rank screen color if EnableOldSpeedChange is enabled");
-            configDebugMode = Config.Bind("General", "DebugMode", false, "Enables Debug Mode");
-            configShowFPS = Config.Bind("General", "ShowFPS", ShowFpsOptions.Enabled, "Shows FPS if Debug Mode is not active");
+            configEnableOldSpeedChange = Config.Bind("Speed Modifiers", "EnableOldSpeedChange", false, "Changes chili/ice speed to be 2x/.5x, respectively");
+            configRankColorOnDoubleSpeed = Config.Bind("Speed Modifiers", "RankColorOnDoubleSpeed", false, "Changes rank screen color if EnableOldSpeedChange is enabled");
+            configShowFPS = Config.Bind("General", "ShowFPS", ShowFpsOptions.Disabled, "Adds an FPS counter on the top right of the screen while in a level.");
             configFiveFourteen = Config.Bind("Funny", "FiveFourteen", false, "514");
-            configHMode = Config.Bind("Funny", "HMode", false, "Replaces \"Samurai.\" text with h");
+            configSamuHrai = Config.Bind("Funny", "SamuHrai", false, "Replaces \"Samurai.\" text with \"h\"");
+            configShowAccuracy = Config.Bind("General", "ShowAccuracy", false, "Adds an accuracy count at the end of a level if Detailed Level Results is enabled.\nDoes not work for 2P.");
             
             if (configEnableOldSpeedChange.Value)
                 Harmony.CreateAndPatchAll(typeof(EnableOldSpeedChange));
@@ -39,17 +38,17 @@ namespace GarbagePlugins
             if (configEnableOldSpeedChange.Value && configRankColorOnDoubleSpeed.Value)
                 Harmony.CreateAndPatchAll(typeof(RankColorOnDoubleSpeed));
 
-            if (configDebugMode.Value)
-                Harmony.CreateAndPatchAll(typeof(DebugMode));
-
             if (configShowFPS.Value != ShowFpsOptions.Disabled)
                 Harmony.CreateAndPatchAll(typeof(ShowFPS));
 
             if (configFiveFourteen.Value)
                 Harmony.CreateAndPatchAll(typeof(FiveFourteen));
 
-            if (configHMode.Value)
-                Harmony.CreateAndPatchAll(typeof(HMode));
+            if (configSamuHrai.Value)
+                Harmony.CreateAndPatchAll(typeof(SamuHrai));
+
+            if (configShowAccuracy.Value)
+                Harmony.CreateAndPatchAll(typeof(ShowAccuracy));
 
             Logger.LogInfo($"Plugin is loaded!");
         }
@@ -82,16 +81,6 @@ namespace GarbagePlugins
 
                 if (RDTime.speed > 1.5f) __instance.rank.color = new Color(1f, 0.2f, 0.2f);
                 if (RDTime.speed < 0.75f) __instance.rank.color = new Color(0.2f, 0.2f, 1f);
-            }
-        }
-
-        private static class DebugMode
-        {
-            [HarmonyPostfix]
-            [HarmonyPatch(typeof(scnBase), "Update")]
-            public static void Postfix()
-            {
-                if (!RDC.debug) RDC.debug = true;
             }
         }
 
@@ -136,7 +125,7 @@ namespace GarbagePlugins
             }
         }
 
-        private static class HMode
+        private static class SamuHrai
         {
             [HarmonyPostfix]
             [HarmonyPatch(typeof(RDString), "Get")]
@@ -147,140 +136,58 @@ namespace GarbagePlugins
                 }
             }
         }
-    }
-}
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection.Emit;
-using BepInEx;
-using BepInEx.Configuration;
-using HarmonyLib;
-using UnityEngine;
-using UnityEngine.UI;
 
-namespace GarbagePlugins
-{
-    [BepInPlugin("com.rhythmdr.garbageplugins", "Garbage Plugins", "1.0.0")]
-    [BepInProcess("Rhythm Doctor.exe")]
-    public class Plugin : BaseUnityPlugin
-    {
-        private static ConfigEntry<bool> configEnableOldSpeedChange;
-        private static ConfigEntry<bool> configRankColorOnDoubleSpeed;
-        private static ConfigEntry<bool> configDebugMode;
-        private static ConfigEntry<ShowFpsOptions> configShowFPS;
-        private static ConfigEntry<bool> configFiveFourteen;
-
-        private enum ShowFpsOptions { Enabled, Legacy, Disabled }
-
-        private void Awake()
+        private static class ShowAccuracy
         {
-            configEnableOldSpeedChange = Config.Bind("General", "EnableOldSpeedChange", true, "Changes chili/ice speed to be 2x/.5x, respectively");
-            configRankColorOnDoubleSpeed = Config.Bind("General", "RankColorOnDoubleSpeed", true, "Changes rank screen color if EnableOldSpeedChange is enabled");
-            configDebugMode = Config.Bind("General", "DebugMode", false, "Enables Debug Mode");
-            configShowFPS = Config.Bind("General", "ShowFPS", ShowFpsOptions.Enabled, "Shows FPS if Debug Mode is not active");
-            configFiveFourteen = Config.Bind("Funny", "FiveFourteen", false, "514");
-            
-            if (configEnableOldSpeedChange.Value)
-                Harmony.CreateAndPatchAll(typeof(EnableOldSpeedChange));
+            private static int[] hits = {0, 0, 0, 0, 0, 0}; // 40, 80, 120, 160, 200, miss
 
-            if (configEnableOldSpeedChange.Value && configRankColorOnDoubleSpeed.Value)
-                Harmony.CreateAndPatchAll(typeof(RankColorOnDoubleSpeed));
-
-            if (configDebugMode.Value)
-                Harmony.CreateAndPatchAll(typeof(DebugMode));
-
-            if (configShowFPS.Value != ShowFpsOptions.Disabled)
-                Harmony.CreateAndPatchAll(typeof(ShowFPS));
-
-            if (configFiveFourteen.Value)
-                Harmony.CreateAndPatchAll(typeof(FiveFourteen));
-
-            Logger.LogInfo($"Plugin is loaded!");
-        }
-
-        private void OnDestroy()
-        {
-            Harmony.UnpatchAll();
-        }
-
-        private static class EnableOldSpeedChange
-        {
             [HarmonyPrefix]
-            [HarmonyPatch(typeof(scnGame), "StartTheGame")]
-            public static bool Prefix(ref float speed)
+            [HarmonyPatch(typeof(scnGame), "Start")]
+            public static void Prefix()
             {
-                if (speed > 1f) speed = 2f;
-                if (speed < 1f) speed = 0.5f;
-                return true;
+                hits = new int[] {0, 0, 0, 0, 0, 0};
             }
-        }
 
-        private static class RankColorOnDoubleSpeed
-        {
             [HarmonyPostfix]
-            [HarmonyPatch(typeof(HUD), "ShowAndSaveRank")]
+            [HarmonyPatch(typeof(scrPlayerbox), "Pulse")]
+            public static void Postfix(float timeOffset)
+            {
+                int num = Math.Abs((int) ((double) timeOffset * 1000.0 / 40));
+                if (num >= 5) hits[5]++;
+                else hits[num]++;
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(HUD), "ShowRankDescription")]
             public static void Postfix(HUD __instance)
             {
-                var levelType = __instance.game.currentLevel.levelType;
-                if (levelType == LevelType.Boss || levelType == LevelType.Challenge) return;
-
-                if (RDTime.speed > 1.5f) __instance.rank.color = new Color(1f, 0.2f, 0.2f);
-                if (RDTime.speed < 0.75f) __instance.rank.color = new Color(0.2f, 0.2f, 1f);
+                if (GC.showAbsoluteOffsets)
+                {
+                    if (!GC.twoPlayerMode)
+                    {
+                        // myseif writes "disgusting code", asked to leave rdl
+                        double num = (hits[0] + hits[1] * 0.9 + hits[2] * 0.75 + hits[3] * 0.5 + hits[4] * 0.25) / (hits[0] + hits[1] + hits[2] + hits[3] + hits[4] + hits[5]) * 100;
+                        __instance.resultsSingleplayer.text = __instance.resultsSingleplayer.text + "\nAccuracy: " + Math.Round(num, 2).ToString() + "%";
+                        __instance.resultsSingleplayer.gameObject.SetActive(true);
+                    }
+                }
+                else 
+                {
+                    __instance.resultsSingleplayer.gameObject.SetActive(false);
+                }
             }
-        }
 
-        private static class DebugMode
-        {
-            [HarmonyPostfix]
-            [HarmonyPatch(typeof(scnBase), "Update")]
-            public static void Postfix()
-            {
-                if (!RDC.debug) RDC.debug = true;
-            }
-        }
-
-        private static class ShowFPS
-        {
-            [HarmonyPostfix]
+            // debug
+            /*[HarmonyPostfix]
             [HarmonyPatch(typeof(scnGame), "Update")]
             public static void Postfix(scnGame __instance)
             {
                 if (!RDC.debug){
                     __instance.debugText.gameObject.SetActive(true);
-
-                    switch (configShowFPS.Value)
-                    {
-                        case ShowFpsOptions.Enabled:
-                            if (scnGame.fps == 0.0) scnGame.fps = 1f / Time.unscaledDeltaTime;
-                            scnGame.fps = scnGame.fps * 0.99f + (1f / Time.unscaledDeltaTime) * 0.01f;
-                            __instance.debugText.text = "" + string.Format("<color=#ffffff>FPS:</color> <color={0}>{1}</color>", (object) (scnGame.fps < 30 ? "#ff0000" : (scnGame.fps < 50 ? "#ffff00" : "#00ff00")), (object) (int) scnGame.fps);
-                            break;
-                        case ShowFpsOptions.Legacy:
-                            int num = Mathf.RoundToInt(1f / Time.unscaledDeltaTime);
-                            __instance.debugText.text = "" + string.Format("<color=#ffffff>FPS:</color> <color={0}>{1}</color>", (object) (num < 30 ? "#ff0000" : (num < 50 ? "#ffff00" : "#00ff00")), (object) num);
-                            break;
-                    }
-                    
+                    __instance.debugText.text += string.Format("\n{0}, {1}, {2}, {3}, {4}, {5}", (object) (int) hits[0], (object) (int) hits[1], (object) (int) hits[2], (object) (int) hits[3], (object) (int) hits[4], (object) (int) hits[5]);
                     __instance.currentLevel.Update();
                 }
-            }
-        }
-
-        private static class FiveFourteen
-        {
-            [HarmonyPostfix]
-            [HarmonyPatch(typeof(HUD), "ShowRankDescription")]
-            public static void Postfix(HUD __instance)
-            {
-                string str = "Mistakes: 514\n514 early + 514 late = 514 offset frames";
-                __instance.resultsSingleplayer.text = str;
-                __instance.resultsSingleplayer.gameObject.SetActive(true);
-                __instance.description.text = "514/514";
-                __instance.description.gameObject.SetActive(true);
-            }
+            }*/
         }
     }
 }
->>>>>>> 684b5795fdb9d66e7e9f0795bdd6baa62cf17e7c
