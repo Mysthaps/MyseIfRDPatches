07778dd941e148319efe0ad9a5f9f70fb73771a0
MyseIf <helluser0@gmail.com>
2024-11-19 19:50:16 +0700

    ver bump

--- Main.cs
+++ Main.cs
@@ -8,7 +8,7 @@ using HarmonyLib;
 
 namespace MyseIfRDPatches
 {
-    [BepInPlugin("com.rhythmdr.myseifrdpatches", "MyseIf's RD Patches", "2.4.3")]
+    [BepInPlugin("com.rhythmdr.myseifrdpatches", "MyseIf's RD Patches", "2.4.4")]
     [BepInProcess("Rhythm Doctor.exe")]
     public class Main : BaseUnityPlugin
     {
